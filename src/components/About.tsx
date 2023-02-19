b3454fdefa2484653cc4e9e00b6c0975cc17d2b6
Marshall Burns <marshallbcodes@gmail.com>
2023-02-19 09:50:58 -0600

    Update styling/re-install dependency

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,28 +2,45 @@ import React from 'react'
 import chicago from '../assets/images/city-of-wind.jpg'
 import code from '../assets/images/hello-world.jpg'
 import cali from '../assets/images/san-diego.jpg'
+
+
 export default function About() {
   return (
     <>
-      <div id='aboutContainer' className='topicContainer'>
-
-        <div id='chapterOne' className='about'>
-          <h2 className='aboutMeHeading' id='cityOfWindHeading'>The City of Wind</h2>
-          <p><img id='imgOfChicago'  src={chicago} alt='The city of Chicago' />Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis aut id hic ea ipsum quam doloremque quaerat nisi numquam quibusdam, assumenda sit, totam omnis nihil quae? Corrupti tempore sapiente totam.</p>
+      
+        <div>
+        <h2 className='aboutMeHeading' id='cityOfWindHeading'>The City of Wind</h2>
+          <div id='chapterOne' className='chapterContainer'>
+            <div className='paragraphContainer' id='chicagoContainer'>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis aut id hic ea ipsum quam doloremque quaerat nisi numquam quibusdam, assumenda sit, totam omnis nihil quae? Corrupti tempore sapiente totam.</p>            
+            </div>
+            <img src={chicago} alt='The city of Chicago' />
+          </div>
         </div>
 
-        <div id='chapterTwo' className='about'>
-          <h2 className='aboutMeHeading' id='californiaHeading'>California</h2>
-          <p><img id='california'  src={cali} alt='The city of San Diego' />Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente aperiam iusto accusamus. Dolores numquam molestias atque modi mollitia accusantium eveniet sit deleniti obcaecati fugiat qui ducimus minima beatae, ratione blanditiis!</p>
-        </div>
+
         
-        <div id='chapterThree' className='about'>
+        <div>
+          <h2 className='aboutMeHeading' id='californiaHeading'>California Dreamin</h2>
+          <div id='chapterTwo' className='chapterContainer'>
+            <div className='paragraphContainer' id='caliParagraphContainer'>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis aut id hic ea ipsum quam doloremque quaerat nisi numquam quibusdam, assumenda sit, totam omnis nihil quae? Corrupti tempore sapiente totam.</p>            
+            </div>
+            <img src={cali} alt='The San Diego Skyline' />
+          </div>
+        </div>
+
+      
+        <div>
           <h2 className='aboutMeHeading' id='helloWorldHeading'>Hello World</h2>
-          <p><img id='helloWorld'  src={code} alt='Code' />Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae alias natus ab necessitatibus molestiae iste libero quo obcaecati dolorum aspernatur, deserunt suscipit voluptates mollitia deleniti est illo harum ratione repellat!</p>
-        </div>
-        
-        
+          <div id='chapterThree' className='chapterContainer'>
+            <div className='paragraphContainer' id='codingParagraphContainer'>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis aut id hic ea ipsum quam doloremque quaerat nisi numquam quibusdam, assumenda sit, totam omnis nihil quae? Corrupti tempore sapiente totam.</p>            
+            </div>
+            <img src={code} alt='Code on a screen' />
+          </div>
       </div>
+      
     </>
   )
 }
